8902604a90b137b5d5084ba61458150f1ba032eb
Nagata <ngtkko@gmail.com>
2018-06-22 13:11:18 +0900

    デプロイ！

--- src/App.js
+++ src/App.js
@@ -4,16 +4,22 @@ import './App.css';
 import { Navbar, Jumbotron, Button } from 'react-bootstrap';
 
 class App extends Component {
+    constructor(props){
+    super(props)
+    this.state = {name:"Nagata"}
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+      <h1 class="App-title">Hello!{this.state.name}</h1>
+      <p>functionalコンポーネントを作成した</p>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <Button bsStyle="primary">Primary</Button><Button bsStyle="danger">Danger</Button>
       </div>
     );
   }
