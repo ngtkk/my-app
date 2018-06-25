e6df2e72782cfb4f851e4cbf7d8f8ecf17145860
Nagata <ngtkko@gmail.com>
2018-06-25 10:17:45 +0900

    緯度経度検索のやりかけ

--- src/App.js
+++ src/App.js
@@ -1,26 +1,69 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import { Navbar, Jumbotron, Button } from 'react-bootstrap';
+import axios from 'axios';
+import SearchForm from './SearchForm';
+import GeocodeResult from './GeocodeResult';
+
+
+
+const GEOCODE_ENDPOINT = 'https://maps.googleapis.com/maps/api/geocode/json';
 
 class App extends Component {
-    constructor(props){
-    super(props)
-    this.state = {name:"Nagata"}
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
   }
+
+  handlePlaceSubmit(place){
+    axios.get(GEOCODE_ENDPOINT,{ params: {address: place} })
+    .then((results) =>{
+      console.log(results);
+      const data = results.data;
+      const result = results.data.results[0];
+      if(data.status === 'OK'){
+        const location = result.geometry.location;
+        this.setState({
+        address: result.formatted_address,
+        lat: location.lat,
+        lng: location.lng,
+      })
+      }else if(data.status === 'ZERO_RESULTS'){
+        this.setState({
+        address: '結果が見つかりませんでした',
+        lat: '***',
+        lng: '***',
+      })
+      }else{
+        this.setState({
+        address: 'エラーです',
+        lat: '***',
+        lng: '***',
+      })
+      }
+  })
+  .catch((error) => {
+    this.setState({
+      address: '通信エラーです',
+      lat: '***',
+      lng: '***',
+    })
+  })
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-      <h1 class="App-title">Hello!{this.state.name}</h1>
-      <p>functionalコンポーネントを作成した</p>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Button bsStyle="primary">Primary</Button><Button bsStyle="danger">Danger</Button>
+       <header className="App-header">
+         <h1>緯度経度検索</h1>
+         <p>希望の位置情報を調べます。</p>
+       </header>
+       <div className="result">
+         <SearchForm onSubmit={place => this.handlePlaceSubmit(place)} />
+         <GeocodeResult address={this.state.address} lat={this.state.lat} lng={this.state.lng} />
       </div>
+     </div>
     );
   }
 }
